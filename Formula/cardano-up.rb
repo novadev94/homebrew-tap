cc90ec04e6a87a608ab9579c0d8bc4676f581697
Nova <novadev94@gmail.com>
2026-08-03 03:44:52 +0700

    cardano-up 0.16.0

--- Formula/cardano-up.rb
+++ Formula/cardano-up.rb
@@ -10,7 +10,7 @@ class CardanoUp < Formula
     raise "Unsupported OS for cardano-up"
   end.freeze
 
-  VERSION = "0.15.0".freeze
+  VERSION = "0.16.0".freeze
 
   ARCH = if Hardware::CPU.arm?
     "arm64"
@@ -20,10 +20,10 @@ class CardanoUp < Formula
 
   # Per-platform checksums
   SHA_TABLE = {
-    ["darwin", "arm64"] => "97339101c7c2fb49f9fe5c0750674597b417427325285606c15ad8699fc6916a",
-    ["darwin", "amd64"] => "16661a7edf15e5b618e5ceb46ca0ddeb0eb3fe3c5c5486b4a9b846d86cb95ecd",
-    ["linux",  "arm64"] => "f39f3fb719698f69395a537b521abccf7c280991d25401a86d4f40dbfa2975b9",
-    ["linux",  "amd64"] => "5efb8751b52a3eb02a4c767de562ad00e92e58fce3e69e9f50fcc34f7eaa75b0",
+    ["darwin", "arm64"] => "3d5bbc4713f3a5206daba535635ab72d3cbba8c81697131afbd27429199fe674",
+    ["darwin", "amd64"] => "450091d7b8f775284ddf77b40b7bf2c986a34fcb2730cc8cc17353ba63bfe63c",
+    ["linux",  "arm64"] => "62adc9770f182f9c42d0f0a77334797d407e2812ffb8bb21bfae64ca9cc9b3dd",
+    ["linux",  "amd64"] => "fb5072c5ae9434a9c6a7c1f6e7c6124f698c63a2d768b18ce6831df94bab5e66",
   }.freeze
 
   url "https://github.com/blinklabs-io/cardano-up/releases/download/v#{VERSION}/cardano-up-v#{VERSION}-#{OSN}-#{ARCH}"
